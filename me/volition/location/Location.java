cf6b06976f38bc57e607c46e1fdd6def6cec19d1
mccloskeybr <brendan.mccloskey@gmail.com>
2016-02-03 15:26:26 -0500

    Repush

--- me/volition/location/Location.java
+++ me/volition/location/Location.java
@@ -1,7 +1,32 @@
 package me.volition.location;
 
+import me.volition.entity.Entity;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 /**
  * Created by Demerzel on 2/3/16.
  */
-public class Location {
+public abstract class Location {
+
+    private ArrayList<Entity> entities;
+
+    public Location(String name, int x, int y) {
+        entities = new ArrayList<>();
+    }
+
+    public void addEntity(Entity entity){
+        entities.add(entity);
+    }
+
+    public ArrayList<Entity> getEntities(){
+        return entities;
+    }
+
+    public void setEntities(ArrayList<Entity> entities) {
+        this.entities = entities;
+    }
+
+    public abstract void render(Graphics g);
 }
---
+++ me/volition/util/Animator.java
@@ -0,0 +1,7 @@
+package me.volition.util;
+
+/**
+ * Created by mccloskeybr on 2/3/16.
+ */
+public class Animator {
+}

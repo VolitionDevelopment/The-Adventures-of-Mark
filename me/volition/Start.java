6a1b5584cdda5bde405529c5c0085c8f4bef2e6f
mccloskeybr <brendan.mccloskey@gmail.com>
2016-02-04 15:40:02 -0500

    Registering new fonts!
    fonts p2

--- me/volition/Start.java
+++ me/volition/Start.java
@@ -2,12 +2,15 @@ package me.volition;
 
 import me.volition.state.StateManager;
 import me.volition.state.menu.MainMenu;
+import me.volition.util.FontManager;
 
 /**
  * Created by Demerzel on 2/3/16.
  */
 public class Start {
     public static void main(String[] args) {
+
+        new FontManager().registerFont("/me/volition/assets/font/DTM-Sans.otf");
         StateManager.setCurrentState(StateManager.MAIN_MENU_INDEX);
         new Main();
     }
--- me/volition/util/FontManager.java
+++ me/volition/util/FontManager.java
@@ -18,5 +18,5 @@ public class FontManager {
             e.printStackTrace();
         }
     }
-    
+
 }

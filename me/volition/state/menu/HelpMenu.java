513ff2b886df26c3d98801fb6fb06eb7b6f2f30e
mccloskeybr <brendan.mccloskey@gmail.com>
2016-02-04 22:54:06 -0500

    Help menu visual update
    black background

--- me/volition/state/menu/HelpMenu.java
+++ me/volition/state/menu/HelpMenu.java
@@ -1,5 +1,7 @@
 package me.volition.state.menu;
 
+import me.volition.*;
+import me.volition.Window;
 import me.volition.state.StateManager;
 import me.volition.util.ImageManager;
 
@@ -18,5 +20,13 @@ public class HelpMenu extends BottomTextMenu {
     public void select(int index) {
         if (index == 0)
             StateManager.setCurrentState(StateManager.MAIN_MENU_INDEX);
+    }
+
+    @Override
+    public void render(Graphics g){
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, Window.WINDOW_WIDTH, Window.WINDOW_HEIGHT);
+
+        super.render(g);
     }
 }
